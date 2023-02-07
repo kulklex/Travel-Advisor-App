fcba104d4df187c49e7bfa0c656520440d8d10f0
kulklex <kulklex@gmail.com>
2023-02-07 06:29:06 +0100

    Components Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { CssBaseline, Grid } from '@mui/material'
 import React from 'react'
+import Header from './Components/Header/Header'
+import Map from './Components/Map/Map'
+import List from './Components/List/List'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <CssBaseline/>
+      <Header/>
+      <Grid container spacing={3} style={{ width: '100%' }} >
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   )
 }
