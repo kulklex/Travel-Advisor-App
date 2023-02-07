fb4d46d484f3ff4ce52d16760b8e57f5a4a22f35
kulklex <kulklex@gmail.com>
2023-02-07 07:25:09 +0100

    Navbar setup

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Search } from '@mui/icons-material';
+import { AppBar, Box, InputBase, Toolbar, Typography } from '@mui/material';
+import { Autocomplete } from '@react-google-maps/api';
+import useStyles from './styles';
 
 const Header = () => {
+    const classes = useStyles()
     return (
-        <h1>
-            Header
-        </h1>
+        <AppBar position='static'>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' className={classes.title}>Travel Advisory</Typography>
+                <Box display='flex'>
+                    <Typography variant='65' className={classes.title}>Explore new places</Typography>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <Search/>
+                            </div>
+                            <InputBase placeholder='Search...' className={{root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/styles';
+
+import { createTheme } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const theme = createTheme()
+root.render(<ThemeProvider theme={theme}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+</ThemeProvider>);
 
 reportWebVitals();
