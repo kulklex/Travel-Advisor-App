cc947d400fdb94d76c37045e647ae66d0712a354
kulklex <kulklex@gmail.com>
2023-02-09 09:43:08 +0100

    Api Call Setup

---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+import { toast } from "react-toastify"
+
+
+export const getPlacesData = async ({type, sw, ne}) => {
+    try {
+        const res = await  axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+            params: {
+                bl_latitude: sw?.lat,
+                bl_longitude: sw?.lng,
+                tr_longitude: ne?.lng,
+                tr_latitude: ne?.lat,
+              },
+              headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+                'X-RapidAPI-HOST': 'travel-advisor.p.rapidapi.com',
+              },
+            })
+            console.log(res)
+        const {data} = res
+        return data
+    } catch (error) {
+        if(error.response){
+            toast.error(`Could not get places...                                ${error.response?.data?.message}`)
+        }
+        console.error(error)
+    }
+}
